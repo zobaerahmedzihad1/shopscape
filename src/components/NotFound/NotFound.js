3160629274c98415db050191f3b37c739d97fda1
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-04-04 09:11:37 +0600

    toggle navbar and not Found page.

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './NotFound.css'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-     return (
-          <div>
-               <h2>Page Not Found.</h2>
-               <h3>404</h3>
-          </div>
-     );
+  return (
+    <div className="error">
+      <h2>Page Not Found.</h2>
+      <h3>404</h3>
+
+      <Link className="backBtn" to="/">BACK HOME</Link>
+    </div>
+  );
 };
 
 export default NotFound;
