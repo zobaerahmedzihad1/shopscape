1b2b2876960e194bf1363c82bbb1ffb8bf24e80e
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-04-04 09:41:12 +0600

    Page not found 404 design complete.

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
+import notFound from "../../images/notFound.gif";
 
 const NotFound = () => {
   return (
     <div className="error">
-      <h2>Page Not Found.</h2>
-      <h3>404</h3>
-
-      <Link className="backBtn" to="/">BACK HOME</Link>
+      <img src={notFound} alt="" />
+      <div>
+        <Link className="backBtn" to="/">
+          BACK HOME
+        </Link>
+      </div>
     </div>
   );
 };
