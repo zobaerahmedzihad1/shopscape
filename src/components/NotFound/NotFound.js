365d7f6e695981968671c61f46edea7d5082eeea
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-04-04 06:31:44 +0600

    shopscape project setup complete.

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+     return (
+          <div>
+               <h2>Page Not Found.</h2>
+               <h3>404</h3>
+          </div>
+     );
+};
+
+export default NotFound;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reviews.css'
+
+const Reviews = () => {
+     return (
+          <div>
+               <h3>This is review.</h3>
+          </div>
+     );
+};
+
+export default Reviews;
